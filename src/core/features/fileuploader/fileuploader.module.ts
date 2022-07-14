01d9862ea2f6ae1b0c21f1a2d6549199bf58dd8f
Sebastian <kuehn.sebastian@gmail.com>
2022-07-14 10:33:50 +0200

    Update fileuploader.module.ts

--- src/core/features/fileuploader/fileuploader.module.ts
+++ src/core/features/fileuploader/fileuploader.module.ts
@@ -40,13 +40,13 @@ export const CORE_FILEUPLOADER_SERVICES: Type<unknown>[] = [
 
                 CoreFileUploaderDelegate.registerHandler(CoreFileUploaderAudioHandler.instance);
                 CoreFileUploaderDelegate.registerHandler(CoreFileUploaderCameraHandler.instance);
-                //CoreFileUploaderDelegate.registerHandler(CoreFileUploaderVideoHandler.instance);
+                CoreFileUploaderDelegate.registerHandler(CoreFileUploaderVideoHandler.instance);
 
-                if (CoreApp.isIOS()) {
+                //if (CoreApp.isIOS()) {
                     CoreFileUploaderDelegate.registerHandler(CoreFileUploaderAlbumHandler.instance);
-                }else{
+                //}else{
                     CoreFileUploaderDelegate.registerHandler(CoreFileUploaderFileHandler.instance);
-                }
+                //}
 
             },
         },
