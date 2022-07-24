e7cb72ea0575e4af566c8ca7099f3fb835aa96f8
Sebastian <kuehn.sebastian@gmail.com>
2022-07-24 22:29:36 +0200

    Update fileuploader.ts

--- src/core/features/fileuploader/services/fileuploader.ts
+++ src/core/features/fileuploader/services/fileuploader.ts
@@ -735,38 +735,36 @@ export class CoreFileUploaderProvider {
             console.log("fileNameFullPath: "+file.fullPath);
             console.log("fileNamenativeURL: "+file.nativeURL);
 
+            let promise = new Promise((resolve, reject) => {
+
+                var name = Math.round(+new Date()/1000);
+
+                let test = VideoEditor.transcodeVideo(
+                    success, // success cb
+                    error, // error cb
+                    {
+                        fileUri: file.nativeURL,
+                        outputFileName: name.toString(),
+                        videoBitrate: 5000000, // optional, bitrate in bits, defaults to 9 megabit (9000000)
+                        fps: 30, // optional (android only), defaults to 30
+                    })
+
+                console.log("promise1 test: "  +test);
+            });
+
+            let result2 = await promise;
+            console.log("promise1 output: "  +result2);
+            console.log("promise1 promise: " +promise);
+
             try {
-
-                let promise = new Promise((resolve, reject) => {
-
-                    var name = Math.round(+new Date()/1000);
-
-                    let test = VideoEditor.transcodeVideo(
-                        success, // success cb
-                        error, // error cb
-                        {
-                            fileUri: file.nativeURL,
-                            outputFileName: name.toString(),
-                            videoBitrate: 5000000, // optional, bitrate in bits, defaults to 9 megabit (9000000)
-                            fps: 30, // optional (android only), defaults to 30
-                        })
-
-                    console.log("promise1 test: "  +test);
-                });
-
-
-
-                console.log("promise1 output: "  +result2);
-                console.log("promise1 promise: " +promise);
-
 
             } catch (error) {
                 this.logger.error('Error while transcoding file', file.nativeURL, error);
 
                 throw new CoreError(Translate.instant('core.errorinvalidresponse'));
             }
-            
-              let result2 = await promise;
+
+
 
 
         } else {
@@ -788,6 +786,8 @@ export class CoreFileUploaderProvider {
             fileEntry = await CoreFile.getExternalFile(path);
 
         }
+
+
 
         // Now upload the file.
         const extension = CoreMimetypeUtils.getFileExtension(fileName!);
